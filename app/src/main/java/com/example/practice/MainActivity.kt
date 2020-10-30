d1aab482f319140f2a96eff16cdd9c924c5b5ceb
NaHui999 <hui999hui@naver.com>
2020-10-31 02:20:36 +0900

    add grid

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -1,13 +1,49 @@
 package com.example.practice
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var RecyclerAdapter: RecyclerAdapter
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.appbar,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.change_grid->{
+                RecyclerAdapter.changeViewType=1
+                main_rcv.apply {
+                    layoutManager=GridLayoutManager(this@MainActivity,2)
+                }
+            }
+            R.id.change_linear->{
+                RecyclerAdapter.changeViewType=0
+                main_rcv.apply{
+                    layoutManager=LinearLayoutManager(this@MainActivity)
+                }
+            }
+            R.id.logout->{
+                val intent_main=Intent(this,MainActivity::class.java)
+                val pref=getSharedPreferences("pref_login", Context.MODE_PRIVATE)
+                val editor=pref.edit()
+                editor.remove("id")
+                editor.remove("pw")
+                editor.commit()
+                startActivity(intent_main)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,16 +52,30 @@ class MainActivity : AppCompatActivity() {
 
         main_rcv.adapter = RecyclerAdapter
         main_rcv.layoutManager = LinearLayoutManager(this) //이 레이아웃을 격자형으로든 바꿀 수 있다.
+        /*
+        RecyclerAdapter.changeViewType = 1
+        main_rcv.apply {
+            layoutManager = GridLayoutManager(this@MainActivity, 2)
+        }
+
+        RecyclerAdapter.changeViewType=0
+        main_rcv.apply{
+            layoutManager=LinearLayoutManager(this@MainActivity)
+        }
+
+         */
 
         RecyclerAdapter.data = mutableListOf(
-                RecyclerData("이름","나희정","2020/10/1","이름에 대한 자세한 설명"),
-                RecyclerData("나이","22","2020/10/2","나이에 대한 자세한 설명"),
-                RecyclerData("파트","안드로이드","2020/10/3","파트에 대한 자세한 설명"),
-                RecyclerData("GitHub","www.github.com/nahui999","2020/10/4","깃헙에 대한 자세한 설명"),
-                RecyclerData("Blog","None","2020/10/5","블로그에 대한 자세한 설명"),
-                RecyclerData("Sopt","www.sopt.org","2020/10/6","솝트에 대한 자세한 설명")
+            RecyclerData("이름", "나희정", "2020/10/1", "이름에 대한 자세한 설명"),
+            RecyclerData("나이", "22", "2020/10/2", "나이에 대한 자세한 설명"),
+            RecyclerData("파트", "안드로이드", "2020/10/3", "파트에 대한 자세한 설명"),
+            RecyclerData("GitHub", "www.github.com/nahui999", "2020/10/4", "깃헙에 대한 자세한 설명"),
+            RecyclerData("Blog", "None", "2020/10/5", "블로그에 대한 자세한 설명"),
+            RecyclerData("Sopt", "www.sopt.org", "2020/10/6", "솝트에 대한 자세한 설명")
         )
         RecyclerAdapter.notifyDataSetChanged()
 
     }
+
+
 }
--- app/src/main/java/com/example/practice/RecyclerAdapter.kt
+++ app/src/main/java/com/example/practice/RecyclerAdapter.kt
@@ -3,6 +3,7 @@ package com.example.practice
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
@@ -10,35 +11,35 @@ import androidx.recyclerview.widget.RecyclerView
 class RecyclerAdapter (private val context:Context) : RecyclerView.Adapter<RecyclerViewHolder>(){
     var data= mutableListOf<RecyclerData>()
 
-    //var changeViewType=0
+    var changeViewType=0//
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
-      /*  var view=
+        var view=
             when (viewType){
                 1->{
-                    LayoutInflater.from(context).inflate(R.layout.item_list_recycler,parent,false)
+                    LayoutInflater.from(context).inflate(R.layout.item_list_grid,parent,false)
                 }
 
                 2->{
-                    LayoutInflater.from(context).inflate(R.layout.item_list_grid,parent,false)
+                    LayoutInflater.from(context).inflate(R.layout.item_list_recycler,parent,false)
                 }
 
                 else->{
-                    LayoutInflater.from(context).inflate(R.layout.item_list_grid,parent,false)
+                    LayoutInflater.from(context).inflate(R.layout.item_list_recycler,parent,false)
                 }
 
-            }*/
-        val view=LayoutInflater.from(context).inflate(R.layout.item_list_recycler,parent,false)
+            }
+        //val view=LayoutInflater.from(context).inflate(R.layout.item_list_recycler,parent,false)
         return RecyclerViewHolder(view)
     }
 
     override fun getItemCount(): Int {
         return data.size //ppt에서 :Int =data.size랑 같은 의미. 줄일수 있음.
     }
-    /*
+    /**/
     override fun getItemViewType(position: Int): Int {
         return changeViewType
-    }*/
+    }
 
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
         holder.onBind(data[position])
